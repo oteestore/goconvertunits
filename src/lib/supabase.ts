1aea0f3e5dd7ccda055dd607155f66de6dd848ff
oteestore <oteestore@Gmail.com>
2025-03-01 20:15:29 +0000

    Deleted Supabase connection

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -2,8 +2,9 @@ import { createClient } from "@supabase/supabase-js";
 import { Database } from "../types/supabase";
 
 // Get Supabase URL and key from environment variables
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = "https://shaorebfoqxvwsmtfvjj.supabase.co";
+const supabaseAnonKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNoYW9yZWJmb3F4dndzbXRmdmpqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA4MzQ2MTksImV4cCI6MjA1NjQxMDYxOX0.oiDaWstW6QTHs3S9ROsn3a3LcuwcWOKM5TgNg3lQUCc";
 
 // Create the Supabase client
 export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey);
